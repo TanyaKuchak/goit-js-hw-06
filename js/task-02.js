38846aaef0fe26ec7723d389c5f485ec771017b5
Tanya Kuchak <tanyakuchak@ukr.net>
2022-09-07 13:36:03 +0300

    Update task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listRef = document.querySelector('#ingredients');
+const itemIngredients = ingredients.map(option => {
+  const itemEl = document.createElement('li');
+itemEl.classList.add('item');
+// ingredients.forEach(ingredients);
+itemEl.textContent = option;
+console.log(itemEl);
+listRef.append(itemEl)
+})
+
+
+
